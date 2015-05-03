6f4441ec914ce22cf854937b68c8e451c0b01b37
mplatvoet <mplatvoet@gmail.com>
2015-05-03 11:26:22 +0200

    restructured

---
+++ src/main/kotlin/progress-jvm.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2015 Mark Platvoet<mplatvoet@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package nl.mplatvoet.komponents.progress
+
+import java.text.DecimalFormat
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.properties.ReadOnlyProperty
+
+
+public fun concreteProgressControl() : ProgressControl = JvmProgress()
+
+
+private class JvmProgress() : ProgressControl, Progress {
+    private val children = ConcurrentHashMap<JvmProgress, Double>()
+    private val callbacks = ConcurrentLinkedQueue<Progress.() -> Unit>()
+    private val atomicVal = AtomicReference(0.0)
+
+    override val progress: Progress
+        get() = this
+
+    override fun markAsDone() {
+        value = 1.0
+    }
+
+    public override val done: Boolean
+        get() = value >= 1.0
+
+
+    public override var value: Double
+        get() = atomicVal.get()
+        set(suggestedValue) {
+            //checking whether this Progress object is managed by children is not thread safe.
+            //it's just a way to catch misuse of the API
+            if (!children.isEmpty()) throw IllegalStateException("children manage the state of this Progress object")
+            if (suggestedValue !in (0.0..1.0)) throw OutOfRangeException("[$value] must be within bounds (0.0 .. 1.0)")
+
+
+            var notify: Boolean
+            do {
+                val currentVal = atomicVal.get()
+                notify = currentVal != suggestedValue
+            } while (!atomicVal.compareAndSet(currentVal, suggestedValue))
+            if (notify) notifyUpdate()
+        }
+
+
+
+
+    override fun createChild(weight: Double): ProgressControl {
+        if (weight < 0.0) throw IllegalArgumentException("weight can not be negative")
+        val child = JvmProgress()
+        children.put(child, weight)
+        child.update { updateValue() }
+        return child
+    }
+
+    private fun updateValue() {
+        var notify: Boolean
+        do {
+            val currentVal = atomicVal.get()
+            val newValue = calculateProgressFromChildren()
+            notify = newValue != value
+        } while (!atomicVal.compareAndSet(currentVal, newValue))
+
+        if (notify) notifyUpdate()
+    }
+
+    private fun calculateProgressFromChildren(): Double {
+        var totalWeight = 0.0
+        var totalWeightValue = 0.0
+        for ((p, w) in children) {
+            totalWeight += w
+            totalWeightValue += p.value * w
+        }
+        return if (totalWeight > 0.0 && totalWeightValue > 0.0) totalWeightValue / totalWeight else 0.0
+    }
+
+
+    private fun notifyUpdate() {
+        callbacks.forEach { body -> this.body() }
+    }
+
+    override fun update(notifyOnAdd: Boolean, body: Progress.() -> Unit) {
+        //Could miss an event, should record what's been called already
+        if (notifyOnAdd) this.body()
+        callbacks add body
+    }
+
+}
+
+public class OutOfRangeException(msg: String, cause: Throwable? = null) : IllegalArgumentException(msg, cause)
--- src/test/kotlin/example/Example.kt
+++ src/test/kotlin/example/Example.kt
@@ -1,27 +1,29 @@
 package example
 
-import nl.mplatvoet.kotlin.komponents.progress.Progress
-import nl.mplatvoet.kotlin.komponents.progress.percentage
+import nl.mplatvoet.komponents.progress.OutOfRangeException
+import nl.mplatvoet.komponents.progress.progressControl
+import java.text.DecimalFormat
+import kotlin.properties.ReadOnlyProperty
 
 fun main(args: Array<String>) {
-    val progress = Progress()
-    progress.onUpdate { p->
-        println("${p.percentage}%")
+    val control = progressControl()
+    control.progress.update {
+        println("${value.percentage}%")
     }
 
-    val sub1 = progress.child(0.1)
-    val sub2 = progress.child(5)
-    val sub2sub1 = sub2.child()
-    val sub2sub2 = sub2.child()
-    val sub3 = progress.child()
-    val sub4 = progress.child(2)
+    val sub1 = control.createChild(0.1)
+    val sub2 = control.createChild(5.0)
+    val sub2sub1 = sub2.createChild()
+    val sub2sub2 = sub2.createChild()
+    val sub3 = control.createChild()
+    val sub4 = control.createChild(2.0)
 
     sub1.value = 0.25
     sub1.value = 0.50
     sub1.value = 0.75
     sub1.value = 1.0
 
-    sub2sub1.completed = true
+    sub2sub1.markAsDone()
     sub2sub2.value = 0.5
     sub2sub2.value = 1.0
 
@@ -34,5 +36,19 @@ fun main(args: Array<String>) {
     sub4.value = 0.50
     sub4.value = 0.75
     sub4.value = 1.0
+}
+
+private val percentageFormat by ThreadLocalVal { DecimalFormat("##0.00") }
+
+val Double.percentage: String
+    get() = if (this in (0.0..1.0)) percentageFormat.format(this * 100) else throw OutOfRangeException("[$this] must be within bounds (0.0 .. 1.0)")
+
+
+private class ThreadLocalVal<T>(private val initializer: () -> T) : ReadOnlyProperty<Any?, T> {
+    private val threadLocal = object : ThreadLocal<T>() {
+        override fun initialValue(): T = initializer()
+    }
+
+    public override fun get(thisRef: Any?, desc: PropertyMetadata): T = threadLocal.get() : T
 }
 
